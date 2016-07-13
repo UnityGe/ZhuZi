dd9bb8bc05b2147ec064ebad4069acea9068d019
Yongye <1014290873@qq.com>
2016-07-13 17:31:26 +0800

    提交
    柱子

---
+++ DestroyScriptf.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyScriptf : MonoBehaviour {
+	void OnEnble(){
+
+		Invoke ("Destroy", 12f);
+	}
+	void Destroy(){
+	
+		gameObject.SetActive (false);
+	}
+	void OnDisable(){
+	
+		CancelInvoke ("Destroy");
+	}
+	//xiugaidaima
+	//修改操作事例
+}
